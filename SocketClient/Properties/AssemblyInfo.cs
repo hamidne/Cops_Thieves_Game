26f96fa912dcf33d76ebe6e9accb9da89ddf7749
HamidNE <hamidne@mail.ru>
2018-06-27 22:20:58 +0430

    Big Update for serve and add multi connection

--- SocketClient/Properties/AssemblyInfo.cs
+++ SocketClient/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SocketServer")]
+[assembly: AssemblyTitle("SocketClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SocketServer")]
+[assembly: AssemblyProduct("SocketClient")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e45a69a2-0e3c-49fc-9357-bebb2d2de45b")]
+[assembly: Guid("ae24181c-c192-45a6-a15c-2fd1f957c394")]
 
 // Version information for an assembly consists of the following four values:
 //
