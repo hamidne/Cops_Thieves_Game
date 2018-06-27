65783715cc2d1bc6e474fd959b95edaa98f3b83a
HamidNE <hamidne@mail.ru>
2018-06-27 15:34:47 +0430

    update server with multi thered

--- SocketServer/Program.cs
+++ SocketServer/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace SocketServer
 {
@@ -12,12 +13,49 @@ namespace SocketServer
         static void Main(string[] args)
         {
             TcpListener serverSocket = new TcpListener(8888);
-            int requestCount = 0;
             TcpClient clientSocket = default(TcpClient);
+            int counter = 0;
+
             serverSocket.Start();
-            Console.WriteLine(" >> Server Started");
-            clientSocket = serverSocket.AcceptTcpClient();
-            Console.WriteLine(" >> Accept connection from client");
+            Console.WriteLine(" >> " + "Server Started");
+
+            counter = 0;
+            while (true)
+            {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine(" >> " + "Client No:" + Convert.ToString(counter) + " started!");
+                handleClinet client = new handleClinet();
+                client.startClient(clientSocket, Convert.ToString(counter));
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine(" >> " + "exit");
+            Console.ReadLine();
+        }
+    }
+
+    //Class to handle each client request separatly
+    public class handleClinet
+    {
+        TcpClient clientSocket;
+        string clNo;
+        public void startClient(TcpClient inClientSocket, string clineNo)
+        {
+            this.clientSocket = inClientSocket;
+            this.clNo = clineNo;
+            Thread ctThread = new Thread(doChat);
+            ctThread.Start();
+        }
+        private void doChat()
+        {
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[10025];
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+            string rCount = null;
             requestCount = 0;
 
             while ((true))
@@ -26,27 +64,23 @@ namespace SocketServer
                 {
                     requestCount = requestCount + 1;
                     NetworkStream networkStream = clientSocket.GetStream();
-                    byte[] bytesFrom = new byte[10025];
-                    int bytesRead = networkStream.Read(bytesFrom, 0, bytesFrom.Length);
-                    string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom, 0, bytesRead);
+                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
                     dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
-                    Console.WriteLine(" >> Data from client -> " + dataFromClient);
-                    string serverResponse = "Last Message from client -> " + dataFromClient;
-                    Byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                    Console.WriteLine(" >> " + "From client-" + clNo + dataFromClient);
+
+                    rCount = Convert.ToString(requestCount);
+                    serverResponse = "Server to clinet(" + clNo + ") " + rCount;
+                    sendBytes = Encoding.ASCII.GetBytes(serverResponse);
                     networkStream.Write(sendBytes, 0, sendBytes.Length);
                     networkStream.Flush();
                     Console.WriteLine(" >> " + serverResponse);
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine(ex.ToString());
+                    Console.WriteLine(" >> " + ex.ToString());
                 }
             }
-
-            clientSocket.Close();
-            serverSocket.Stop();
-            Console.WriteLine(" >> exit");
-            Console.ReadLine();
         }
-    }
+    } 
 }
