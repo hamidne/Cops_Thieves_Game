5caf2a5da2a5967b5ddf033d624ac330db53cd4c
Erfan <erfanzekri@gmail.com>
2018-06-28 10:54:05 +0430

    Implement Player Class

--- TesT_UI/Form1.cs
+++ TesT_UI/Form1.cs
@@ -12,6 +12,7 @@ namespace TesT_UI
 {
     public partial class Form1 : Form
     {
+        Player cop, thief;
         int level = 0;
         public Form1()
         {
@@ -26,8 +27,12 @@ namespace TesT_UI
                 int n = int.Parse(textBox1.Text.ToString());
                 for (int i = 0; i < n; i++)
                 {
-                    flowLayoutPanel1.Controls.Add(btn(i + 1));
+                    flowLayoutPanel1.Controls.Add(btn(i));
                 }
+                cop = new Player(5, Int32.Parse(textBox1.Text) / 5, 1);
+                cop.Type = true;
+                thief = new Player(5, Int32.Parse(textBox1.Text) / 5, 2);
+                thief.Type = true;
                 label1.Text = "Set Police Position : ";
                 textBox1.Text = null;
                 level++;
@@ -37,6 +42,11 @@ namespace TesT_UI
                 Control[] Btn = flowLayoutPanel1.Controls.Find("Tile " + textBox1.Text.ToString(), true);
                 Btn[0].Text = "Police";
                 Btn[0].ForeColor = Color.Blue;
+                //Send Request to server To Create new Player with posX,posY and get number and playground response (TO DO)
+                cop.positionX = Int32.Parse(textBox1.Text) % 5;
+                cop.positionY = Int32.Parse(textBox1.Text) / 5;
+                cop.playGround[cop.positionX, cop.positionY] = cop.Number;
+
                 label1.Text = "Set Thief Position : ";
                 textBox1.Text = null;
                 level++;
@@ -45,7 +55,14 @@ namespace TesT_UI
             {
                 Control[] Btn = flowLayoutPanel1.Controls.Find("Tile " + textBox1.Text.ToString(), true);
                 Btn[0].Text = "Thief";
+                //Send Request to server To Create new Player with posX,posY and get number and playground response (TO DO)
                 Btn[0].ForeColor = Color.Red;
+                thief.positionX = Int32.Parse(textBox1.Text) % 5;
+                thief.positionY = Int32.Parse(textBox1.Text) / 5;
+                thief.playGround[thief.positionX, thief.positionY] = thief.Number;
+                thief.playGround[cop.positionX, cop.positionY] = cop.Number;
+                cop.playGround[thief.positionX, thief.positionY] = thief.Number;
+
                 label1.Visible = false;
                 textBox1.Visible = false;
                 btn_set.Visible = false;
---
+++ TesT_UI/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesT_UI
+{
+    class Player
+    {
+        public int[,] playGround;
+        public int positionX;
+        public int positionY;
+        public int Number;
+        public bool isYourTurn;
+        public bool Type;
+        // Type is False -> is Thief
+        // Type is True -> is Cop
+
+        Player()
+        {
+
+        }
+        public Player(int x, int y, int num)
+        {
+            playGround = new int[x,y];
+            Number = num;
+            isYourTurn = true;
+        }
+    }
+}
